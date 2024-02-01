5d65ab2861985a7aef2c8cd217a4a789aa87a933
dw <1798827828@qq.com>
2024-02-01 02:44:43 -0500

    fix

--- src/lora/run_classinfo_mutation.sh
+++ src/lora/run_classinfo_mutation.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+#SBATCH --partition=contrib-gpuq
+#SBATCH --qos=ksun
+#SBATCH --job-name=classinfo_mutation
+#SBATCH --output=/projects/ksun3/%u/sbatch_log/%x-%N-%j.out
+#SBATCH --error=/projects/ksun3/%u/sbatch_log/%x-%N-%j.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:A100.80gb:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=480G
+#SBATCH --export=ALL
+#SBATCH --time=5-00:00:00
+
+set echo
+umask 0022
+
+# to see ID and state of GPUs assigned
+nvidia-smi
+
+module load gnu10
+
+source 	~/Anaconda/etc/profile.d/conda.sh
+conda activate dschat
+
 accelerate launch llama_sft.py \
     --model_name_or_path codellama/CodeLlama-7b-hf \
     --data_path /projects/ksun3/dwu25/apr_datasets_processing/java_mutation/data/classinfo_mutation \
